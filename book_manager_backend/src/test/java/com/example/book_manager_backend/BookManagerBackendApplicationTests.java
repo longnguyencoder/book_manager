0b58173023b7b731c8d02dd066b0723e8d583f33
longnguyencoder <long0398481719@gmail.com>
2025-03-12 15:33:14 +0700

    update pom: lombok map

---
+++ book_manager_backend/src/test/java/com/example/book_manager_backend/BookManagerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.book_manager_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookManagerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
